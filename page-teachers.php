ae140e53a821a90031b5f4461d7430591c821ec3
Cashman1396 <Chrisashman96@gmail.com>
2021-11-11 17:19:11 -0500

    added effects and code to teachers

--- page-teachers.php
+++ page-teachers.php
@@ -16,13 +16,39 @@ get_header();
 <div class="lessonPlans">
     <img src="http://localhost:10088/wp-content/uploads/2021/11/clip.png">
 
+    <h1>Lessons <br>
+    Plans</h1>
+
+    <p>Lorem ipsum dolor sit amet, <br>
+     consetetur sadipscing elitr, <br>
+      sed diam nonumy eirmod </p>
+
+
 </div>
 
+<a href="tools-tips">
 <div class="toolTips">
+    <img src="http://localhost:10088/wp-content/uploads/2021/11/lightbulb.png">
+
+    <h1>Tools & <br> 
+    Tips</h1>
+
+    <p>Lorem ipsum dolor sit amet, <br>
+     consetetur sadipscing elitr, <br>
+      sed diam nonumy eirmod </p>
 
 </div>
+</a>
 
 <div class="relateRead">
+    <img src="http://localhost:10088/wp-content/uploads/2021/11/Book.png">
+
+    <h1>Related <br>
+    Readings</h1>
+
+    <p>Lorem ipsum dolor sit amet, <br>
+    consetetur sadipscing elitr, <br>
+    sed diam nonumy eirmod </p>
 
 </div>
 </div> 
@@ -32,6 +58,44 @@ get_header();
     <img src="http://localhost:10088/wp-content/uploads/2021/11/coolerdots.png">
 </div>
 
+<div class="Contact">
+		<img class="ktdgLogo" src="http://localhost:10088/wp-content/uploads/2021/11/ktdg-logo.png">	
+		
+		<div class="contactHeading">
+			<h6>
+				Contact
+			</h6>
+	</div> 
+
+
+		<div class="Email">
+			<img class="Mail" src="http://localhost:10088/wp-content/uploads/2021/11/Icon-material-email.png">
+			<p>info@kidsthatdogood.com</p>
+			
+		</div>
+
+		<div class="Connect">
+			<h6>Connect</h6>
+
+			<img class="connectIcon" src="http://localhost:10088/wp-content/uploads/2021/11/connectIcons.png">
+			
+	   </div>
+
+	   <div class="Inform">
+		   <h6>Information</h6>
+
+		   <p>Terms and Conditions <br>
+			Privacy Policy</p>
+	   </div>
+
+	   <div class="Tax">
+		   <p class="numKids">Kids That Do Good is a <span class="numBold">501C3</span></p>
+		   <p class="numTax">Tax ID# <span class="numBold">45-5182066.</span></p>
+	   </div>
+		
+</div>
+
+
 
 
 
@@ -39,4 +103,3 @@ get_header();
 
 
 <?php get_footer(); ?>
-</div>
---
+++ page-tools-tips.php
@@ -0,0 +1,60 @@
+<?php 
+
+/* Template Name: Tools & Tips */
+
+get_header(); 
+?>
+
+<div class="zigShadow">
+<div class="bannerAssets">
+        <img src="http://localhost:10088/wp-content/uploads/2021/11/lightbulb.png">
+
+        <h2>Tools & Tips</h2>
+
+        <p>Learn the tips and tricks to a running a successful charity.  </p>
+    </div>
+</div>
+
+<div class="ZigZag">
+    <img src="http://localhost:10088/wp-content/uploads/2021/11/ZiggyZaggy.png">
+    
+</div>
+
+
+<div class="Charities">
+    <div class="Study">
+        <img src="http://localhost:10088/wp-content/uploads/2021/11/Studying-e1636659605938.png">
+        <div class="studyGreen">
+            <h4>How To Create an Effective <br>
+            Poster or Flyer for Your Event</h4>
+
+            <p>A detailed guide on to create an <br>
+             effective poster or flyer.</p>
+        </div>
+    </div>
+
+    <div class="Soup">
+        <img src="http://localhost:10088/wp-content/uploads/2021/11/Soupcans-e1636662497213.png">
+        <div class="soupGreen">
+            <h4>A 5 Step Plan for a Winning <br>
+            Food Drive</h4>
+
+            <p>Learn how to plan an effective food <br> drive with this detailed guide.</p>
+        </div>
+    </div>
+
+    <div class="Cupcake">
+        <img src="http://localhost:10088/wp-content/uploads/2021/11/cupcakes-e1636662677697.png">
+        <div class="cupcakeGreen">
+
+        </div>
+    
+    </div>
+
+
+
+
+</div>
+
+
+
