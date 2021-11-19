3334cda1fa46e22151825b955782ad23e8caa9e6
Cashman1396 <Chrisashman96@gmail.com>
2021-11-19 02:33:07 -0500

    did work on kid section

--- page-kids.php
+++ page-kids.php
@@ -5,8 +5,49 @@
 get_header(); 
 ?>
 
+<div class="kidShadow"></div>
+<div class="kidsHeader">
+    <img src="http://localhost:10088/wp-content/uploads/2021/11/ZigZag-all-1.png">
+</div>
 
-<p>hi</p>
+
+<div class="Contact">
+		<img class="ktdgLogo" src="http://localhost:10088/wp-content/uploads/2021/11/ktdg-logo.png">	
+		
+		<div class="contactHeading">
+			<h6>
+				Contact
+			</h6>
+	</div> 
+
+
+		<div class="Email">
+			<img class="Mail" src="http://localhost:10088/wp-content/uploads/2021/11/Icon-material-email.png">
+			<p>info@kidsthatdogood.com</p>
+			
+		</div>
+
+		<div class="Connect">
+			<h6>Connect</h6>
+
+			<img class="connectIcon" src="http://localhost:10088/wp-content/uploads/2021/11/connectIcons.png">
+			
+	   </div>
+
+	   <div class="Inform">
+		   <h6>Information</h6>
+
+		   <p>Terms and Conditions <br>
+			Privacy Policy</p>
+	   </div>
+
+	   <div class="Tax">
+		   <p class="numKids">Kids That Do Good is a <span class="numBold">501C3</span></p>
+		   <p class="numTax">Tax ID# <span class="numBold">45-5182066.</span></p>
+	   </div>
+		
+</div>
+
 
 
 
