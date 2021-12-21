380f9407d558d29f772a777f02ff4346fe5a4035
Cashman1396 <Chrisashman96@gmail.com>
2021-12-21 13:15:40 -0500

    did two related readings

--- page-related-readings.php
+++ page-related-readings.php
@@ -24,12 +24,32 @@ get_header();
 
 
 <div class="givingTree">
+
+<div class="treeBorder">
+        <h6>The Giving Tree <br>
+        By: Shel Silverstein</h6>
+
+        <p>Reading Level: 4-5 years old</p>
+    </div>
     <img src="http://localhost:10088/wp-content/uploads/2021/12/tree2.0.png">
+
+  
 
 </div>
 
 
 <div class="everyOne">
+
+    <div class="everyBorder">
+        <h6>
+        If Everybody Did <br>
+        By: Jo Ann Stover
+       </h6>
+
+       <p>Reading Level: 1-3 years old</p>
+
+
+   </div>
     <img src="http://localhost:10088/wp-content/uploads/2021/12/everybodyone.png">
 
 
