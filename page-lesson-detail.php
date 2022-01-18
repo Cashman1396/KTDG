210f7c04819dc28bf6e4a1c4dee8c5c1946d2109
Cashman1396 <Chrisashman96@gmail.com>
2022-01-18 11:31:18 -0500

    working on lesson detail

--- page-lesson-detail.php
+++ page-lesson-detail.php
@@ -5,14 +5,19 @@
 get_header(); 
 ?>
 
+<div class="zigShadow">
+<div class="bannerAssets">
+        <img src="http://localhost:10088/wp-content/uploads/2021/11/lightbulb.png">
 
-<p>hi</p>
+        <h2>What is an Endangered Species</h2>
 
-
-
-
-
-
-
-<?php get_footer(); ?>
+        
+    </div>
 </div>
+
+
+
+
+
+
+
